fd6c3f4d0bd07ce6db9d7334fcbcea71ad14311b
Kaique Lopes <41844709+Kaique-Lopes@users.noreply.github.com>
2021-06-26 20:31:26 -0300

    ConversationsController

---
+++ App_Chat_MVVM/App_Chat_MVVM/Controller/ConversationsController.swift
@@ -0,0 +1,12 @@
+//
+//  ConversationsController.swift
+//  App_Chat_MVVM
+//
+//  Created by Kaique Lopes on 26/06/21.
+//
+
+import UIKit
+
+class ConversationsController: UIViewController {
+    
+}
